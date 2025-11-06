1e6a43e3b72502232eb9af11a63eb3099d51ab1f
MJR <martynryan947@gmail.com>
2025-11-06 15:06:50 +0400

    Update train_model.py

--- src/training/train_model.py
+++ src/training/train_model.py
@@ -5,33 +5,31 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import joblib
 
-# -------------------------------
+
 # Resolve project root directory
-# -------------------------------
+--
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 
 PROCESSED_DATA_PATH = os.path.join(BASE_DIR, "data", "processed", "insurance_processed.csv")
 MODEL_DIR = os.path.join(BASE_DIR, "models")
 MODEL_PATH = os.path.join(MODEL_DIR, "insurance_model.pkl")
 
-# -------------------------------
+---
 # Load processed dataset
-# -------------------------------
+
 print(f" Loading processed dataset from: {PROCESSED_DATA_PATH}")
 df = pd.read_csv(PROCESSED_DATA_PATH)
 
-# -------------------------------
 # Prepare features and target
-# -------------------------------
+
 X = df.drop("charges", axis=1)
 y = df["charges"]
 
 # Train/test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# -------------------------------
-# Train model
-# -------------------------------
+#Train model
+
 print(" Training RandomForestRegressor model...")
 model = RandomForestRegressor(n_estimators=200, random_state=42)
 model.fit(X_train, y_train)
@@ -48,9 +46,9 @@ print(f" Mean Absolute Error: {mae:.2f}")
 print(f" Mean Squared Error: {mse:.2f}")
 print(f" R² Score: {r2:.2f}")
 
-# -------------------------------
+
 # Save model
-# -------------------------------
+
 os.makedirs(MODEL_DIR, exist_ok=True)
 joblib.dump(model, MODEL_PATH)
 
